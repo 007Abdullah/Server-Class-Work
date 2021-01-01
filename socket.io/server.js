6b93f9d140b660de54b560c07771d3d17d9d480b
Muhammad Abdullah <kb337137@gmail.com>
2021-01-01 17:55:07 +0500

    socket

--- socket.io/server.js
+++ socket.io/server.js
@@ -1,11 +1,5 @@
 var users = [
-    { userName: "ali" },
-    { userName: "li" },
-    { userName: "ai" },
-    { userName: "ai" },
-    { userName: "ali" },
-    { userName: "ai" },
-    { userName: "aliadasd" },
+    { username: "ali" },
 ];
 
 
@@ -28,64 +22,26 @@ app.get("/signup", (req, res, next) => {
     res.send("signup response")
 })
 
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 
 app.use("/", express.static(path.resolve(path.join(__dirname, 'public'))))
 
 app.post('/gtdata', (req, res, next) => {
-    // let sass = req.body.name;
     users.push({
-        userName: req.body.name
+        username: req.body.username
     });
-    res.send({
-        check: users,
-        message: "send"
-    });
-    console.log(sass);
+    res.send(users)
+    io.emit("CURRENT", JSON.stringify({
+        username: req.body.username
+    }))
 })
-var server = http.createServer(app);
 
-
-var io = socketIO(server);
-
-
-
-
-io.on('connection', (user) => {
-
-    console.log("user", user);
-
-    console.log("user id: ", user.id);
-
-
-    {
-        users.map((eachUser) => {
-            console.log("Each user", eachUser.userName)
-        })
-    }
-
-    console.log("Name ", users[0].userName)
-
-    user.emit("Name ", users[0].userName)
-
-    users.push(user)
-    setTimeout(function () {
-        {users.map((eachUser) => {
-                console.log("Each user", eachUser.userName)
-            })
-        }
-
-    }, 1000)
-    console.log('a user connected');
-
-});
-
-
-setInterval(function () {
-    io.emit("COMMON_TOPIC", `some comon data: ${new Date().getSeconds()}`)
-}, 3000)
-
+app.get('/gtdata', (req, res, next) => {
+    res.send(users);
+})
 
 server.listen(PORT, () => {
     console.log("server is running on: ", PORT);
